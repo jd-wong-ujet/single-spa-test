d13bc1f6c6ad03382172e05fd018bd404bae8c89
JD Wong <jd.wong@ujet.cx>
2024-12-12 12:34:38 -0800

    initial commit

---
+++ test-root-config/src/UJET-root-config.js
@@ -0,0 +1,43 @@
+import { registerApplication, start } from "single-spa";
+
+registerApplication({
+  name: "@single-spa/welcome",
+  app: () =>
+    import(
+      /* webpackIgnore: true */
+      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+    ),
+  activeWhen: ["/"],
+});
+
+registerApplication({
+  name: "@UJET/test-page",
+  app: () => import(
+    /* webpackIgnore: true */
+    "//localhost:8080/UJET-test-page.js"
+  ),
+  activeWhen: ["/test"],
+})
+
+// registerApplication({
+//   name: "@UJET/previous-calls",
+//   app: () => import(
+//     /* webpackIgnore: true */
+//     "//localhost:8500/UJET-previous-calls.js"
+//   ),
+//   activeWhen: ["/previous-calls"],
+// })
+
+// registerApplication({
+//   name: "@UJET/navbar",
+//   app: () =>
+//     import(
+//       /* webpackIgnore: true */
+//       "@UJET/navbar"
+//     ),
+//   activeWhen: ["/"],
+// });
+
+start({
+  urlRerouteOnly: true,
+});
